d282e92d0d57a181a6b62b590cb99396fb1924ac
Jeet Vora <jeetvora331@gmail.com>
2023-02-19 23:48:24 +0530

    First Commit 20 feb

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+	return (
+		<Html lang="en">
+			<Head>
+				<link
+					href="https://api.fontshare.com/v2/css?f[]=satoshi@1,900,700,500,400&display=swap"
+					rel="stylesheet"
+				/>
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
